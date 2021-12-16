60fcf129ef28d043650e8c38e00c6df206643037
Stephen <teamgreensf@gmail.com>
2021-12-16 10:11:09 -0800

    Ensure we only inject button on the main WO page

--- customButton.js
+++ customButton.js
@@ -1,17 +1,28 @@
-fetch(chrome.runtime.getURL("customButton.html")).then(r => r.text()).then(html => {
-    // Attempt to inject the "Create Work Tag" button into the page
-    try {
-        document.getElementById("dropdownMenu").parentElement.insertAdjacentHTML('beforebegin', html);
-    } catch (error) {
-        console.log("Could not inject button");
-        return;
-    }
+function injectButton() {
+    fetch(chrome.runtime.getURL("customButton.html")).then(r => r.text()).then(html => {
+        // Attempt to inject the "Create Work Tag" button into the page
+        try {
+            document.getElementById("dropdownMenu").parentElement.insertAdjacentHTML('beforebegin', html);
+        } catch (error) {
+            console.log("Could not inject button");
+            return;
+        }
+    
+        // Set onclick() of the injected button
+        if (document.getElementById("generateTag") != null) {
+            let button = document.getElementById("generateTag");
+            button.onclick = function() {
+                chrome.runtime.sendMessage({ type: "generateTag" });
+            };
+        }
+    });
+}
 
-    // Set onclick() of the injected button
-    if (document.getElementById("generateTag") != null) {
-        let button = document.getElementById("generateTag");
-        button.onclick = function() {
-            chrome.runtime.sendMessage({ type: "generateTag" });
-        };
-    }
-});
+// Only inject our custom button if we are on the main WO page
+if (!location.href.includes("formName=")   &&
+    !location.href.includes("pageType=")   &&
+    !location.href.includes("pagetype=")   &&
+    !location.href.includes("form=")       &&
+    !location.href.includes("customView=") &&
+    !location.href.includes("viewName="))
+    injectButton();
