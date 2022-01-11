9a00632a92e8a3718a3a88b044c4c96a3af03112
Stephen <teamgreensf@gmail.com>
2022-01-11 15:31:09 -0800

    Beginning stages of COTS tags

---
+++ customCots.js
@@ -0,0 +1,7 @@
+if ($("span.user_name").text() == "STEPHEN F") {
+    $("table#dataTable tbody tr td:first-child").append(" (<a style=\"color: #F00;\" class=\"testbutton\" href=\"#\">create tag</a>)");
+
+    $("a.testbutton").click(function () {
+        console.log("https://machinesciences.adionsystems.com" + $(this).prev().attr("href"));
+    });
+}
