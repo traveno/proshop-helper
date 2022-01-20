b7c5b13e673972fbb849977d556bc5260cba5414
Stephen <teamgreensf@gmail.com>
2022-01-19 17:08:51 -0800

    Fully functioning PO-wide file renaming Added a progress counter Algo now skips over already "prettified" file names to reduce HTTP calls Renaming requests to the server are now sequential to prevent overload "Make Pretty" button is now always in the correct column Added debug information TODO: Add comments TODO: Refactor doWork() -- not strictly neccessary and it's ambiguous

--- customPO.js
+++ customPO.js
@@ -1,43 +1,67 @@
-$("table.poBody td.clsdHeader:contains(Other Files)").append("<button class=\"btn btn-raised btn-secondary\" type=\"button\" id=\"prettifyPO\" title=\"Prettify this PO\">Make Pretty</button>");
+$("table.poBody td.clsdHeader:contains(Other Files)").append("</br><button class=\"btn btn-raised btn-secondary\" type=\"button\" id=\"prettifyPO\" title=\"Prettify this PO\">Make Pretty</button>");
 
 var numLines = 0;
 var numLinesProcessed = 0;
 
 $("#prettifyPO").click(doWork);
 
-async function doWork() {
+function doWork() {
     let editList = $("td.attValue[align='left'] div.hidden-buttons-wrapper a[title='Edit']");
-    numLines = editList.length;
+    let delayMultiplier = 0;
 
-    $(editList).each(async function() {
-        await delay();
-        fetch("https://machinesciences.adionsystems.com" + $(this).attr("onclick").split("'")[1]).then(res => res.text()).then(html => {
-            let parser = new DOMParser();
-            let editFileDoc = parser.parseFromString(html, "text/html");
+    // Remove unnecessary work
+    $(editList).each(function() {
+        let text = $(this).parent().parent().find("font a").get(0).childNodes[1].nodeValue;
+        if (!text.includes("PO") && !text.includes("PS")) {
+            numLines++;
 
-            let filename = $(editFileDoc).find("form#linkEditForm input").eq(2);
+            renameFile($(this).attr("onclick").split("'")[1], (delayMultiplier++) * 500);
+            debug("renaming " + text);
+        } else
+            debug("skipping " + text);
+    });
+
+    if (numLines == 0)
+        debug("nothing found to rename");
+}
+
+async function renameFile(href, msDelay) {
+    // Delay this function to avoid overloading the server
+    await delay(msDelay);
     
-            $(filename).val(makePretty($(filename).val()));
+    fetch("https://machinesciences.adionsystems.com" + href).then(res => res.text()).then(html => {
+        let parser = new DOMParser();
+        let editFileDoc = parser.parseFromString(html, "text/html");
 
-            $(editFileDoc).find("form#linkEditForm").submit();
+        let filename = $(editFileDoc).find("form#linkEditForm input").eq(2);
 
-            const data = new URLSearchParams(new FormData(editFileDoc.getElementById("linkEditForm")));
+        $(filename).val(makePretty($(filename).val()));
 
-            fetch("https://machinesciences.adionsystems.com" + $(editFileDoc).find("form#linkEditForm").attr("action"), {
-                method: "POST",
-                body: data
-            }).then(function() {
-                numLinesProcessed++;
+        $(editFileDoc).find("form#linkEditForm").submit();
 
-                if (numLinesProcessed == numLines) {
-                    location.reload();
-                }
-            });
+        const data = new URLSearchParams(new FormData(editFileDoc.getElementById("linkEditForm")));
+
+        fetch("https://machinesciences.adionsystems.com" + $(editFileDoc).find("form#linkEditForm").attr("action"), {
+            method: "POST",
+            body: data
+        }).then(function() {
+            numLinesProcessed++;
+
+            if (numLinesProcessed == 1)
+                $("#prettifyPO").after("<br><br><p>Processed " + numLinesProcessed + " files of " + numLines + "...</p>");
+            else 
+                $("#prettifyPO").siblings("p").html("<p>Processed " + numLinesProcessed + " files of " + numLines + "...</p>");
+
+            if (numLinesProcessed == numLines) {
+                location.reload();
+            }
         });
     });
 }
 
-const delay = (ms = 1000) => new Promise(r => setTimeout(r, ms));
+function delay(ms) {
+    return new Promise(resolve => { setTimeout(() => { resolve('') }, ms)});
+}
 
 function makePretty(string) {
     // Check if it's empty, or if we've already prettified the text
