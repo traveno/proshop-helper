ce97b86b2c3ac30552acee68235aaa01a2c8dab1
Stephen <teamgreensf@gmail.com>
2022-02-21 16:28:09 -0800

    Get date function for all modules

--- ts/common.ts
+++ ts/common.ts
@@ -5,3 +5,14 @@ export function delayMs(ms: number = 0) {
 export function debugInfo(subject: string = "Unknown", info: string): void {
     chrome.runtime.sendMessage({ type: "debug", file: subject, info: info });
 }
+
+export function getFullDate(): string {
+    let date = new Date();
+    return date.getFullYear() + "-" +
+           (date.getMonth() + 1) + "-" +
+           date.getDate() + "@" +
+           date.getHours() + "-" +
+           date.getMinutes() + "-" +
+           date.getSeconds() + "-" +
+           date.getMilliseconds();
+}
